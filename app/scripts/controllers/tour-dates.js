b720ac31f870f1230c7423298a1711fb3ffb8ed5
James Pants <jamespants@gmail.com>
2015-02-04 16:31:59 +0100

    need to discard past shows

--- app/scripts/controllers/tour-dates.js
+++ app/scripts/controllers/tour-dates.js
@@ -9,9 +9,20 @@
  */
 angular.module('locasApp').controller('TourDatesCtrl', ['$scope', 'contentfulClient', function ($scope, contentfulClient) {
 
+
+	$scope.currentDate = Date.now();
+
+
 	contentfulClient.entries({'content_type': '2Vy9CNn3HyCeAQAIMIyaya', 'include': 1}).then(function(response){
 		$scope.shows = response;
 		console.log(response);
+
+		// -------------------------------------------------
+		//
+		// Loop through and discard any old shows
+		// 
+		// -------------------------------------------------
+		
 	});
 
 }]);
